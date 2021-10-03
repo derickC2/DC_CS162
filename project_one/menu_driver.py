3fc10643f6607aba1b5349f6651df7861fffb88b
derickC2 <Derick.calder.6041@mail.linnbenton.edu>
2021-10-03 11:51:32 -0700

    Added Goblin class

--- project_one/menu_driver.py
+++ project_one/menu_driver.py
@@ -1,18 +1,16 @@
 import player
+import goblin
 
 def __main__():
-    
     a_player = player.Player()
+    a_goblin = goblin.Goblin()
 
     a_player.inspect_character()
 
-    a_player.protect()
+    a_player.attack(a_goblin)
+    a_goblin.get_health()
+    a_goblin.attack(a_player)
 
-    a_player.inspect_character()
-
-    a_player.protect()
-
-    a_player.inspect_character()
 
 if __name__ == "__main__":
     __main__()
--- project_one/player.py
+++ project_one/player.py
@@ -23,20 +23,32 @@ class Player:
         else: #fortitude
             self.fortitude += 1
 
-    def attack(self):
+    def attack(self, target):
         damage = self.strength + random.randrange(1,5)
         print(f"You do {damage} physical damage!")
-        return damage
+        target.get_attacked(damage)
 
-    def cast(self):
+    def cast(self, target):
         damage = self.intelligence + random.randrange(1,5)
         print(f"You do {damage} magic damage!")
-        return damage
+        target.get_attacked(damage)
 
     def protect(self):
         self.absorb += self.fortitude + random.randrange(1,5)
         print(f"You gain a shield. You will absorb the next {self.absorb} damage!")
-    
+
+    def get_attacked(self, damage):
+        if self.absorb >= damage:
+            self.absorb = self.absorb - damage
+            print("Your shield absorbed the damage")
+            print(f"You have {self.absorb} damage remaining in your shield")
+        elif self.absorb < damage:
+            damage = damage - self.absorb
+            print(f"You absorb {self.absorb} damage, and take {damage} damage.")
+            self.absorb = 0
+            self.health = self.health - damage
+            print(f"You have {self.health} remaining health.")
+        
     def inspect_character(self):
         print(f"Your character is  level {self.level}")
         print(f"You will absorb the next {self.absorb} damage!")
