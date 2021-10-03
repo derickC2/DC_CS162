4aef0e6f5e261b7745bdae50835fb10200e0f35a
derickC2 <Derick.calder.6041@mail.linnbenton.edu>
2021-10-03 11:20:49 -0700

    started added attributes and methods for the play class as well as testing code in the menu_driver

--- project_one/menu_driver.py
+++ project_one/menu_driver.py
@@ -4,10 +4,15 @@ def __main__():
     
     a_player = player.Player()
 
-    print(f"Player strength: {a_player.strength}\n"
-            f"Player Intelligence: {a_player.intelligence}\n"
-            f"Player Fortitude: {a_player.fortitude}")
+    a_player.inspect_character()
 
+    a_player.protect()
+
+    a_player.inspect_character()
+
+    a_player.protect()
+
+    a_player.inspect_character()
 
 if __name__ == "__main__":
     __main__()
--- project_one/player.py
+++ project_one/player.py
@@ -4,8 +4,44 @@ class Player:
 
     def __init__(self):
         self.level = 1
-        self.health = 100
+        self.absorb = 0
 
         self.strength = 5 + random.randrange(0,6)
         self.intelligence = 5 + random.randrange(0,6)
         self.fortitude = 5 + random.randrange(0,6)
+
+        self.health = self.fortitude * 10
+
+    def level_up(self, skill):
+        self.level += 1
+        self.health += self.fortitude
+
+        if skill == 1: #Strength
+            self.strength += 1
+        elif skill == 2: #Intelligence
+            self.intelligence += 1
+        else: #fortitude
+            self.fortitude += 1
+
+    def attack(self):
+        damage = self.strength + random.randrange(1,5)
+        print(f"You do {damage} physical damage!")
+        return damage
+
+    def cast(self):
+        damage = self.intelligence + random.randrange(1,5)
+        print(f"You do {damage} magic damage!")
+        return damage
+
+    def protect(self):
+        self.absorb += self.fortitude + random.randrange(1,5)
+        print(f"You gain a shield. You will absorb the next {self.absorb} damage!")
+    
+    def inspect_character(self):
+        print(f"Your character is  level {self.level}")
+        print(f"You will absorb the next {self.absorb} damage!")
+        print(f"Stats:")
+        print(f"Player health: {self.health}\n"
+              f"Player strength: {self.strength}\n"
+              f"Player Intelligence: {self.intelligence}\n"
+              f"Player Fortitude: {self.fortitude}")
